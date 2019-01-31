5e4f7728d44f344fda46c82f4d06ef14c3f2f6a5
Chell0 <machelgabriel@gmail.com>
2019-01-31 22:02:50 +0300

    Animate when scrolling

--- resources/js/script.js
+++ resources/js/script.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
             $('nav').removeClass('sticky');
         }
     }, {
-        offset: '60px;'
+        offset: '60px'
     });
 
     // Scroll on buttons
@@ -43,6 +43,28 @@ $(document).ready(function () {
                 }
             }
         });
+    });
+
+    // Animations on scroll
+    $('.js--wp-1').waypoint(function (direction) {
+        $('.js--wp-1').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+    $('.js--wp-2').waypoint(function (direction) {
+        $('.js--wp-2').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+    $('.js--wp-3').waypoint(function (direction) {
+        $('.js--wp-3').addClass('animated fadeInUp');
+    }, {
+        offset: '50%'
+    });
+    $('.js--wp-4').waypoint(function (direction) {
+        $('.js--wp-4').addClass('animated pulse');
+    }, {
+        offset: '50%'
     });
 
 });
